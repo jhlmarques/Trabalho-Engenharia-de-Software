5a945edd59643a18b0dc04b7d1e4605bd82e7724
Léo Vasconcelos <leohernandesvasc@gmail.com>
2023-02-02 02:54:38 -0300

    add login page

--- app/main.py
+++ app/main.py
@@ -1,13 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 # Adicionar esse decorador em uma função faz com que o flask associe a função abaixo a
 # um URL. Ex: URL Padrão
-@app.route("/")
-def hello_world():
+@app.route("/", methods=["GET", "POST"])
+def login():
     # render_template renderiza um arquivo html e retorna.
-    return render_template('index.html')
+        return render_template('login.html')
+
+@app.route("/activities", methods=["GET", "POST"])
+def activities():
+        return render_template('activities.html')
 
 if __name__ == "__main__":
     app.run()
